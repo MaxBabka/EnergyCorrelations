d5ebc13d0a164599470187641969316622b9e641
liamhayes <liamthemacmaster@gmail.com>
2020-11-27 18:10:37 -0500

    test1

--- EnergyCorrelations.py
+++ EnergyCorrelations.py
@@ -1,4 +1,8 @@
 # VzZ_bBj8-4sZ1m4VyZ6f
+import quandl
+import numpy
+API_KEY  = "VzZ_bBj8-4sZ1m4VyZ6f"
+quandl.ApiConfig.api_key = API_KEY
 
-
-URL = "https://www.quandl.com/api/v3/datasets/OPEC/ORB.csv?api_key=VzZ_bBj8-4sZ1m4VyZ6f"
+data = quandl.get("EIA/PET_RWTC_D", returns="numpy")
+print(data)
